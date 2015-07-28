cdcbc7f66fffe7f476473a3707e88e299e8acf6f
WackyWalrus <mcculloughjchris@gmail.com>
2015-07-28 18:03:03 -0400

    added favicon images

--- index.php
+++ index.php
@@ -21,10 +21,12 @@ $sodaSql = $mysqli->query("SELECT * FROM sodas ORDER BY name ASC");
 <html>
 	<head>
 		<title><?=$siteName;?> - find or log the nearest store with Faygo!</title>
+		<link rel="icon" href="images/favicon_small.png" />
 		<meta name="description" content="Use this tool to search for or log the nearest store with Faygo soda." />
 		<meta property="og:title" content="<?=$siteName;?> - find or log the nearest store with Faygo!" />
 		<meta property="og:url" content="http://faygosearch.tk/" />
 		<meta property="og:description" content="Use this tool to search for or log the nearest store with Faygo soda." />
+		<meta property="og:image" content="images/favicon_big.png" />
 		<link rel="stylesheet" type="text/css" href="styles.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4RJ-gzdvIohmgHvcU1LnzMyPsaQED45s"></script>
