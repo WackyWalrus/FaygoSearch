ea5441de76fcc63ab39437289e4a4f86a09e9667
WackyWalrus <mcculloughjchris@gmail.com>
2015-08-12 19:35:56 -0400

    moved meta tags into a different file

--- index.php
+++ index.php
@@ -43,13 +43,7 @@ $sodaSql = $mysqli->query("SELECT * FROM sodas ORDER BY name ASC");
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?=$siteName;?> - find or log the nearest store with Faygo!</title>
-		<link rel="icon" href="images/favicon_small.png" />
-		<meta name="description" content="Use this tool to search for or log the nearest store with Faygo soda." />
-		<meta property="og:title" content="<?=$siteName;?> - find or log the nearest store with Faygo!" />
-		<meta property="og:url" content="http://faygosearch.tk/" />
-		<meta property="og:description" content="Use this tool to search for or log the nearest store with Faygo soda." />
-		<meta property="og:image" content="http://faygosearch.tk/images/favicon_big.png" />
+		<?php include 'includes/meta.php'; ?>
 		<link rel="stylesheet" type="text/css" href="styles.css" />
 		<link rel="stylesheet" type="text/css" href="scripts/chosen.min.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
